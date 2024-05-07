a8615cfd805ea97003626765923aa2db84bcff71
Antoine Cadoret <antoinecadoret97@gmail.com>
2024-05-07 17:15:32 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  <header>
+    <div class="flex justify-between items-center p-4 bg-blue-400">
+      <img class="w-12 h-12" :src="logo" alt="logo"/>
+
+      <nav>
+        <RouterLink class="m-2" to="/">Home</RouterLink>
+        <RouterLink class="m-2" to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+</template>
+<script lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import hockeyJersey from '@/assets/image/hockey-jersey.png';
+  export default {
+    data(){
+      return {
+        logo: hockeyJersey
+      }
+    }
+  }
+</script>
