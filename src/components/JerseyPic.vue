6ea309060216468cbe15f3eabe689eeffa8e7468
Antoine Cadoret <antoinecadoret97@gmail.com>
2024-05-13 14:57:47 +0200

    [Build] show jersey by team

---
+++ src/components/JerseyPic.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export default {
+    props: ['jerseyType', 'jerseyAddress']
+  }
+</script>
+
+<template>
+  <div class="jersey m-3">
+    <h2 class="text-center font-bold text-2xl">{{ jerseyType }}</h2>
+    <div v-if="jerseyType === 'Alternates'" class="flex">
+      <img  v-for="address in jerseyAddress" class="w-1/3 m-6" :src="address" alt="jersey pic">
+    </div>
+    <img v-else :src="jerseyAddress" alt="jersey pic"/>
+  </div>
+</template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,23 +1,52 @@
 <template>
-  <div class="about">
-    <h1>{{ team.city }}</h1>
-  </div>
+  <main class="teamPage">
+    <header :class="team.color" class="p-6 flex justify-around items-center rounded-b-lg" >
+      <img :src="imgAddress" :alt="team.fullName" width="120" height="120">
+      <h1 class="text-3xl text-white">{{ team.fullName }}</h1>
+      <img :src="imgAddress" :alt="team.fullName" width="120" height="120">
+    </header>
+    <div>
+      <div class="flex">
+        <JerseyPic class="w-1/2" v-if="team.jerseys" jersey-type="Home" :jersey-address="team.jerseys.home"/>
+        <JerseyPic class="w-1/2" v-if="team.jerseys" jersey-type="Away" :jersey-address="team.jerseys.away"/>
+      </div>
+      <JerseyPic v-if="team.jerseys" jersey-type="Alternates" :jersey-address="team.jerseys.alternates"/>
+    </div>
+  </main>
 </template>
 <script lang="ts">
+  interface Jersey {
+    home: string;
+    away: string;
+    alternates: string[];
+  }
 
-interface Team {
-  color: string;
-  shortName: string;
-  city: string;
-}
+  interface Team {
+    city: string;
+    color: string;
+    fullName: string;
+    jerseys?: Jersey | undefined;
+    shortName: string;
+  }
   import teams from '../assets/teams.json';
+  import JerseyPic from '@/components/JerseyPic.vue';
   
   export default{
     props: ['team'],
+    components:{
+      JerseyPic
+    },
+    data(){
+      return {
+        imgAddress: ""
+      }
+    },
     computed: {
       team(): Team {
         console.log(teams, 'TEAMS', this.$route.params.teamName, this.$route.params.team_name);
-        return teams.teams.filter(team => team.city === this.$route.params.team_name)[0];
+        let value: Team = teams.teams.filter(team => team.city === this.$route.params.team_name)[0];
+        this.imgAddress = "https://assets.nhle.com/logos/nhl/svg/"+value.shortName+"_dark.svg";
+        return value;
       }
     }
   }
